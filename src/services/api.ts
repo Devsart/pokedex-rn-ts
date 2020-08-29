481eb8994d26a4ff0e7514fc817bb617a5166b35
Matheus Sartor <matheus.sartor@poli.ufrj.br>
2020-08-29 12:36:17 -0300

    Memory fixes

--- src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    //aqui vc coloca seu endereço de hospedagem, deve ser o mesmo presente na pasta server
-    baseURL: 'https://pokeapi.co/api/v2/pokemon/'
-})
-
-export default api
